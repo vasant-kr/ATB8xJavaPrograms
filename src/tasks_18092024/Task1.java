94f1f3931104db6f6ede752a389a3d6cd3657d70
Vasu5372 <vasantkumar2101@gmail.com>
2024-09-19 01:26:17 +0530

    Committing tasks for 18092024

--- src/tasks_18092024/Task1.java
+++ src/tasks_18092024/Task1.java
@@ -6,17 +6,18 @@ public class Task1 {
         // Examples of Widening and Narrowing
 
 
-        // Widening
+        // Widening - Implicit
         byte b = 50;
-        //Implicit:
         int a = b;
 
-        //Explicit
+        //Widening - Explicit
         int a1 = (int)b;   // Valid - Explicit Casting - JVM - line 11 and line 12 are same
 
-        // Narrowing
+        // Narrowing - Implicit
         int val = 500;
         //byte b1 = val;    // Invalid - Implicit casting - JVM
+
+        //Narrowing - Explicit
         byte b2 = (byte) val;  // Explicit casting - Loss of data - The data will be stored to byte howmuchever it can hold, rest of the data will not be stored
         System.out.println(b2);  // -12 as only the last 8 bits of the 32 bits of 500 are stored which has the value -12
 
