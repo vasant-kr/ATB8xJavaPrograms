5fd5650c29c0de9c37fac3fec3b67f3b8fc02f00
Vasu5372 <vasantkumar2101@gmail.com>
2024-09-19 01:18:13 +0530

    Committing tasks for 18092024

--- src/task_18092024/Task3.java
+++ src/task_18092024/Task3.java
@@ -3,13 +3,14 @@ package task_18092024;
 public class Task3 {
     public static void main(String[] args) {
         int a = 10;
-        System.out.println(a-- + a-- + a--); // Prints 27
-        System.out.println(a);// Prints 7
+        System.out.println(--a + a++ + a--); // Prints 28
+        System.out.println(a);// Prints 9
 
 
-        // Part A -> a--, Exp = 10, a =9
-        // Part B -> a--, Exp = 9, a =8
-        // Part C -> a--, Exp = 8, a =7
+
+        // Part A -> --a, Exp = 9, a =9
+        // Part B -> a++, Exp = 9, a =10
+        // Part C -> a--, Exp = 10, a =9
 
 
     }
