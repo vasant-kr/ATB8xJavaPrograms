a876a8d00586d08f6b4e8ed135ced2cf65188659
Vasu5372 <vasantkumar2101@gmail.com>
2024-11-19 00:32:01 +0530

    Committing changes

---
+++ src/wrapper/WrapperBasicsIntro.java
@@ -0,0 +1,25 @@
+package wrapper;
+
+public class WrapperBasicsIntro {
+
+    /*
+    Wrapper classes in Java are essential components that allow primitive datatypes to be treated as objects
+    For some extra functions over primitive data types
+    There are no functions/methods in data types. This is why Java is not 100% OOPS
+    By adding a wrapper then we have a lot of methods available
+
+    Basically for boxing and Unboxing:
+
+    Primitive datatypes to Wrapper Classes - Boxing
+    Wrapper classes to primitive datatypes - Unboxing
+
+    Autoboxing : Integer a = 5;
+    Unboxing: int i = a;
+
+
+In Collection framework - We can only use Wrapper classes as we can only use classes and objects
+
+     */
+}
+
+
